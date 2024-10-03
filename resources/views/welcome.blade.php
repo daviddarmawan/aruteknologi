7d76d438c32f2a0f961723df49df9e66a5675be7
Anthonio Stefanus <antoniostefanus0933@gmail.com>
2024-10-03 21:11:20 +0700

    index finish

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,30 +3,15 @@
 
 <head>
 	<title>ARUTEKNOLOGI | Teknologi Digital</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flaticon/css/flaticon.css">
     <link rel="icon" href="/images/logo2.png" type="image/x-icon" />
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700,800,900&display=swap"rel="stylesheet">
-	
-
-	<link rel="stylesheet" href="/css/open-iconic-bootstrap.min.css">
 	<link rel="stylesheet" href="/css/animate.css">
-
-	<link rel="stylesheet" href="/css/owl.carousel.min.css">
-	<link rel="stylesheet" href="/css/owl.theme.default.min.css">
-	<link rel="stylesheet" href="/css/magnific-popup.css">
- 
-	<link rel="stylesheet" href="/css/aos.css">
-	<link rel="stylesheet" href="/css/ionicons.min.css">
-
-	<link rel="stylesheet" href="/css/bootstrap-datepicker.css">
-	<link rel="stylesheet" href="/css/jquery.timepicker.css">
-
-
-	<link rel="stylesheet" href="/css/flaticon.css">
 	<link rel="stylesheet" href="/css/icomoon.css">
-	<link rel="stylesheet" href="/css/styles.css">
+	<link rel="stylesheet" href="/css/ionicons.min.css">
+	<link rel="stylesheet" href="/css/style.css">	
+	<link rel="stylesheet" href="/css/flaticon.css">
 </head>
 
 <body>
@@ -43,8 +28,6 @@
 				<ul class="navbar-nav ml-auto">
 					<li class="nav-item active"><a href="index.html" class="nav-link">Beranda</a></li>
 					<li class="nav-item"><a href="about.html" class="nav-link">Tentang</a></li>
-					<li class="nav-item"><a href="event.html" class="nav-link">Acara</a></li>
-					<li class="nav-item"><a href="sponsors.html" class="nav-link">Sponsor</a></li>
 					<li class="nav-item"><a href="contact.html" class="nav-link">Kontak</a></li>
 					<li class="nav-item"><a href="JoinIEEE.html" class="nav-link">Bergabung</a></li>
 				</ul>
@@ -62,7 +45,7 @@
 				data-scrollax-parent="true">
 				<div class="col-lg-6 col-md-6 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
 					<h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Solusi Cerdas untuk Masa Depan Digital
-						<br><span style="color: #000086;">ARU<span style="color: #000000;">TEKNOLOGI</span></h1>
+						<br><span style="color: #000086;">ARU <span style="color: #000000;">TEKNOLOGI</span></h1>
 				</div>
 			</div>
 		</div>
@@ -73,7 +56,9 @@
 			<div class="row d-flex flex-row justify-content-center align-contents-center">
 				<div class="col-md-3 d-flex align-self-stretch ftco-animate">
 					<div class="media block-6 services d-block">
-						<div class="icon text-center"><span class="flaticon-placeholder"></span></div>
+						<br>
+						<div class="icon text-center"><img src="/images/placeholder.png" alt="Description of image" width="50" height="50"></span></div>
+						<br>
 						<div class="media-body">
 							<h3 class="heading mb-3 text-center">Lokasi</h3>
 							<p>Jakarta, Indonesia</p>
@@ -82,7 +67,9 @@
 				</div>
 				<div class="col-md-3 d-flex align-self-stretch ftco-animate">
 					<div class="media block-6 services d-block">
-						<div class="icon text-center"><span class="flaticon-handshake"></span></div>
+					<br>
+						<div class="icon text-center"><img src="/images/agreement.png" alt="Description of image" width="50" height="50"></span></div>
+						<br>
 						<div class="media-body">
 							<h3 class="heading mb-3 text-center">Klien</h3>
 							<p>29 klien yang telah bergabung</p>
@@ -91,7 +78,9 @@
 				</div>
 				<div class="col-md-3 d-flex align-self-stretch ftco-animate">
 					<div class="media block-6 services d-block">
-						<div class="icon text-center"><span class="flaticon-guest"></span></div>
+					<br>
+						<div class="icon text-center"><img src="/images/closure.png" alt="Description of image" width="50" height="50"></span></div>
+						<br>
 						<div class="media-body">
 							<h3 class="heading mb-3 text-center">Projek</h3>
 							<p>Lebih dari 35 proyek yang berhasil dilaksanakan bersama klien.</p>
@@ -114,7 +103,7 @@
 							<h2 class="mb-4">Tentang Kami</h2>
 						</div>
 
-						<p>Dalam era digital yang terus berkembang, ARUTEKNOLOGI muncul sebagai pelopor dalam solusi teknologi berbasis cloud. Dalam dunia yang semakin terhubung dan cepat berubah, kami menyadari tantangan yang dihadapi bisnis dalam beradaptasi dengan kemajuan teknologi yang pesat. Kami berkomitmen untuk memberdayakan bisnis dengan memanfaatkan kemampuan komputasi awan, memungkinkan mereka untuk beradaptasi dan berkembang di tengah lanskap yang semakin kompleks. Selain itu, kami tidak hanya menyediakan solusi cloud; kami juga menawarkan layanan pemeliharaan menyeluruh yang dirancang untuk memastikan sistem dan aplikasi klien kami tetap optimal dan efisien. Dengan layanan pemeliharaan kami, bisnis dapat fokus pada operasi inti mereka sementara kami mengelola semua aspek teknis dan pemeliharaan infrastruktur cloud mereka.</p>
+						<p>Dalam era digital yang terus berkembang, ARU TEKNOLOGI muncul sebagai pelopor dalam solusi teknologi berbasis cloud. Dalam dunia yang semakin terhubung dan cepat berubah, kami menyadari tantangan yang dihadapi bisnis dalam beradaptasi dengan kemajuan teknologi yang pesat. Kami berkomitmen untuk memberdayakan bisnis dengan memanfaatkan kemampuan komputasi awan, memungkinkan mereka untuk beradaptasi dan berkembang di tengah lanskap yang semakin kompleks. Selain itu, kami tidak hanya menyediakan solusi cloud; kami juga menawarkan layanan pemeliharaan menyeluruh yang dirancang untuk memastikan sistem dan aplikasi klien kami tetap optimal dan efisien. Dengan layanan pemeliharaan kami, bisnis dapat fokus pada operasi inti mereka sementara kami mengelola semua aspek teknis dan pemeliharaan infrastruktur cloud mereka.</p>
 						<p><a href="contact.html" class="btn btn-primary">Contact Us</a></p>
 					</div>
 				</div>
@@ -216,6 +205,11 @@
 				<a href="/"><img src="images/klien/rotibakar.png " class="img-fluid" aalt="" width="150" height="auto"></a>
 			  </div>
 			</div>
+			<div class="col-lg-3 col-md-4 col-xs-6">
+			  <div class="supporter-logo">
+				<a href="/"><img src="images/klien/kopiluwak.jpeg " class="img-fluid" aalt="" width="150" height="auto"></a>
+			  </div>
+			</div>
 		  </div>
 		</div>
 	  </section>
@@ -231,10 +225,9 @@
 						<h2 class="ftco-heading-2">ARU Teknologi</h2>
 						<p>ARUTEKNOLOGI: Pelopor solusi cloud, kami memberdayakan bisnis untuk beradaptasi di era digital.</p>
 						<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-							<li class="ftco-animate"><a href="https://www.linkedin.com/company/ieeedelhissn/"><span class="icon-linkedin"></span></a></li>
-							<li class="ftco-animate"><a href="https://www.facebook.com/ieeedelhissn"><span class="icon-facebook"></span></a></li>
-							<li class="ftco-animate"><a href="https://www.instagram.com/ieeedelhissn/"><span class="icon-instagram"></span></a></li>
-							<li class="ftco-animate"><a href="https://www.youtube.com/channel/UCI70Hlz4AHmfgY_xWadTtig"><span class="icon-youtube"></span></a></li>
+						<li class="ftco-animate"><a href="https://www.linkedin.com/company/ieeedelhissn/"><img src="/images/linkedin.png" alt="LinkedIn" width="45" height="45" class="icon-image"></a></li>
+							<li class="ftco-animate"><a href="https://www.facebook.com/ieeedelhissn"><img src="/images/facebook.png" alt="LinkedIn" width="45" height="45" class="icon-image"></a></li>
+							<li class="ftco-animate"><a href="https://www.instagram.com/ieeedelhissn/"><img src="/images/instagram.png" alt="LinkedIn" width="45" height="45" class="icon-image"></a></li>
 						</ul>
 					</div>
 				</div>
@@ -254,10 +247,9 @@
 						<h2 class="ftco-heading-2">Contact Us</h2>
 						<div class="block-23 mb-3">
 							<ul>
-								<li><span class="icon icon-map-marker"></span><span class="text">Jakarta, Indoneisa</span></li>
-                                <li><span class="icon icon-map-marker"></span><span class="text">+62 822-5828-0517</span></li>
-								<li><a href="#"><span class="icon icon-envelope"></span><span
-											class="text">aruteknologi@gmail.com</span></a></li>
+							<li><img src="/images/placeholder.png" width="25" height="25" class="icon-image" style="margin-right: 18px;"><span class="text">Jakarta, Indonesia</span></li>
+							<li><img src="/images/whatsapp.png" width="20" height="20" class="icon-image" style="margin-right: 22px;"><span class="text">+62 822-5828-0517</span></li>
+                            <li><img src="/images/email.png" width="22" height="22" class="icon-image" style="margin-right: 22px;"><span class="text">aruteknologi@gmail.com</span></li>
 							</ul>
 						</div>
 					</div>
